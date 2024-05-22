50bac0a173f56f4912f3c41e96ad7ccf58af069a
Dax Harris <matteovh@gmail.com>
2024-05-22 10:47:46 -0400

    add item button menu

--- cognita_client/src/views/collections/CollectionsView.tsx
+++ cognita_client/src/views/collections/CollectionsView.tsx
@@ -1,13 +1,80 @@
-import { ActionIcon, Box, ScrollArea } from "@mantine/core";
-import { IconPlus } from "@tabler/icons-react";
+import { ActionIcon, Box, ScrollArea, Stack, Tooltip } from "@mantine/core";
+import { useClickOutside, useDisclosure } from "@mantine/hooks";
+import {
+    IconFileText,
+    IconFolder,
+    IconPhoto,
+    IconPlus,
+} from "@tabler/icons-react";
+import { useTranslation } from "react-i18next";
 
 export function CollectionsView() {
+    const [itemAdd, { toggle: toggleItemAdd, close: closeItemAdd }] =
+        useDisclosure(false);
+    const itemsRef = useClickOutside(() => closeItemAdd());
+    const { t } = useTranslation();
     return (
         <Box className="collections-main">
             <ScrollArea className="collections-scroll" p="sm"></ScrollArea>
-            <ActionIcon radius="xl" size="xl" className="add-item-button">
-                <IconPlus />
-            </ActionIcon>
+            <Box
+                className={"add-item-menu" + (itemAdd ? " active" : "")}
+                ref={itemsRef}
+            >
+                <Stack className="add-item-stack" gap="sm" align="center">
+                    <Stack className="add-item-items" gap="xs" align="center">
+                        <Tooltip
+                            position="left"
+                            label={t("views.collections.add_item.folder")}
+                            color="dark"
+                        >
+                            <ActionIcon
+                                radius="xl"
+                                size="lg"
+                                className="add-item-option"
+                                variant="light"
+                            >
+                                <IconFolder size={20} />
+                            </ActionIcon>
+                        </Tooltip>
+                        <Tooltip
+                            position="left"
+                            label={t("views.collections.add_item.file")}
+                            color="dark"
+                        >
+                            <ActionIcon
+                                radius="xl"
+                                size="lg"
+                                className="add-item-option"
+                                variant="light"
+                            >
+                                <IconFileText size={20} />
+                            </ActionIcon>
+                        </Tooltip>
+                        <Tooltip
+                            position="left"
+                            label={t("views.collections.add_item.image")}
+                            color="dark"
+                        >
+                            <ActionIcon
+                                radius="xl"
+                                size="lg"
+                                className="add-item-option"
+                                variant="light"
+                            >
+                                <IconPhoto size={20} />
+                            </ActionIcon>
+                        </Tooltip>
+                    </Stack>
+                    <ActionIcon
+                        radius="xl"
+                        size="xl"
+                        className="add-item-button"
+                        onClick={toggleItemAdd}
+                    >
+                        <IconPlus />
+                    </ActionIcon>
+                </Stack>
+            </Box>
         </Box>
     );
 }
