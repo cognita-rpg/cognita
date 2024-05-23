45032b43dd7a08f5355e8267d4df786b28bc30db
Dax Harris <matteovh@gmail.com>
2024-05-22 20:17:53 -0400

    update plugin hooks

--- cognita_client/src/util/plugin/index.ts
+++ cognita_client/src/util/plugin/index.ts
@@ -1,12 +1,17 @@
 import { useContext, useMemo } from "react";
 import { PluginProvider } from "./PluginProvider";
-import { PluginContext, PluginContextType, ResolvedExportType } from "./types";
+import {
+    PluginContext,
+    PluginContextType,
+    ResolvedPluginExport,
+    FullExport,
+} from "./types";
 import { PluginExport, PluginManifest } from "../../types/plugin";
 import createLoadRemoteModule from "@paciolan/remote-module-loader";
-import { useApi } from "../api";
+import { ApiContextModel, useApi } from "../api";
 
 export { PluginProvider };
-export type { PluginContextType };
+export type { PluginContextType, ResolvedPluginExport, FullExport };
 
 export function usePlugins(): PluginContextType {
     return useContext(PluginContext);
@@ -41,81 +46,97 @@ export function usePlugin(name: string): PluginManifest | null {
     }
 }
 
-export function usePluginExport<TExport extends PluginExport>(
+function getPluginExport<TExport extends PluginExport>(
+    plugins: PluginContextType,
+    api: ApiContextModel,
     name: string,
     exportName: string
-): (() => Promise<ResolvedExportType<TExport>>) | null {
+): FullExport<TExport> {
+    const loadRemoteModule =
+        plugins.state === "ready"
+            ? createLoadRemoteModule(plugins.dependencies)
+            : async (..._: any[]) => {};
+    const plugin =
+        plugins.state === "ready" ? plugins.plugins[name] ?? null : null;
+    const exported = plugin ? plugin.exports[exportName] ?? null : null;
+
+    let result: ResolvedPluginExport<TExport>;
+    if (exported && plugins.state === "ready") {
+        switch (exported.type) {
+            case "component":
+                result = async () => {
+                    const module = await loadRemoteModule(
+                        `/api/plugins/${name}/export/${exportName}`
+                    );
+                    if (Object.keys(module).includes(exported.function)) {
+                        return module[exported.function];
+                    } else {
+                        return null;
+                    }
+                };
+                break;
+            case "function":
+                result = async () => {
+                    const module = await loadRemoteModule(
+                        `/api/plugins/${name}/export/${exportName}`
+                    );
+                    if (Object.keys(module).includes(exported.function)) {
+                        return module[exported.function];
+                    } else {
+                        return null;
+                    }
+                };
+                break;
+            case "asset":
+                result = async () =>
+                    `/api/plugins/${name}/export/${exportName}` as any;
+                break;
+            case "json":
+                result = (async () => {
+                    if (api.state === "ready") {
+                        const result = await api.request<object>({
+                            path: `/plugins/${name}/export/${exportName}`,
+                        });
+                        if (result.success) {
+                            return result.data;
+                        } else {
+                            return null;
+                        }
+                    } else {
+                        return null;
+                    }
+                }) as any;
+                break;
+        }
+    } else {
+        result = null;
+    }
+
+    return {
+        pluginName: name,
+        exportName,
+        metadata: exported as any,
+        resolver: result,
+    };
+}
+
+export function usePluginExport(
+    plugin: string,
+    ...exports: string[]
+): { [key: string]: FullExport } {
     const plugins = usePlugins();
     const api = useApi();
-    const loadRemoteModule = useMemo(
-        () =>
-            plugins.state === "ready"
-                ? createLoadRemoteModule(plugins.dependencies)
-                : async (..._: any[]) => {},
-        [plugins.state]
-    );
-    const plugin = useMemo(() => {
-        if (plugins.state === "ready") {
-            return plugins.plugins[name] ?? null;
-        } else {
-            return null;
-        }
-    }, [plugins.state]);
-    const exported = useMemo(() => {
-        if (plugin) {
-            return plugin.exports[exportName] ?? null;
-        } else {
-            return null;
-        }
-    }, [plugin]);
-    const result = useMemo(() => {
-        if (exported && plugins.state === "ready") {
-            switch (exported.type) {
-                case "component":
-                    return async () => {
-                        const module = await loadRemoteModule(
-                            `/api/plugins/${name}/export/${exportName}`
-                        );
-                        if (Object.keys(module).includes(exported.function)) {
-                            return module[exported.function];
-                        } else {
-                            return null;
-                        }
-                    };
-                case "function":
-                    return async () => {
-                        const module = await loadRemoteModule(
-                            `/api/plugins/${name}/export/${exportName}`
-                        );
-                        if (Object.keys(module).includes(exported.function)) {
-                            return module[exported.function];
-                        } else {
-                            return null;
-                        }
-                    };
-                case "asset":
-                    return async () =>
-                        `/api/plugins/${name}/export/${exportName}`;
-                case "json":
-                    return async () => {
-                        if (api.state === "ready") {
-                            const result = await api.request<object>({
-                                path: `/plugins/${name}/export/${exportName}`,
-                            });
-                            if (result.success) {
-                                return result.data;
-                            } else {
-                                return null;
-                            }
-                        } else {
-                            return null;
-                        }
-                    };
-            }
-        } else {
-            return null;
-        }
-    }, [exported, plugins.state]);
 
-    return result;
+    const results = useMemo(() => {
+        const output: {
+            [key: string]: FullExport;
+        } = {};
+
+        for (const exp of exports) {
+            output[exp] = getPluginExport(plugins, api, plugin, exp);
+        }
+
+        return output;
+    }, [plugin, exports, plugins.state, api.state]);
+    return results;
 }
--- cognita_client/src/util/plugin/types.ts
+++ cognita_client/src/util/plugin/types.ts
@@ -33,3 +33,13 @@ export type ResolvedExportType<TExport extends PluginExport> =
         : TExport extends PluginJSONExport
         ? object
         : never;
+
+export type ResolvedPluginExport<TExport extends PluginExport = PluginExport> =
+    | (() => Promise<ResolvedExportType<TExport>>)
+    | null;
+export type FullExport<TExport extends PluginExport = PluginExport> = {
+    pluginName: string;
+    exportName: string;
+    metadata: TExport | null;
+    resolver: ResolvedPluginExport<TExport>;
+};
